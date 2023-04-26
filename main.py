4b6fc7f3e705a3b66e492d2bcf1e6d91b06fae12
DevPrinceK <princesamuelpks@gmail.com>
2023-04-26 20:11:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -5,22 +6,37 @@ import numpy as np
 
 app = FastAPI()
 
+# open api to all origins and platforms
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class HouseFeatures(BaseModel):
     features: list
 
-# this loads the model from the dir and return it to the predict function
+
 def load_model():
+    '''this loads the model from the dir and return it to the predict function'''
     with open('models/model.pkl', 'rb') as file:
         model = pickle.load(file)
     return model
 
 
-# this function takes the features and returns the prediction
 def predict(features):
+    '''this function takes the features and returns the prediction'''
     model = load_model()
     prediction = model.predict(features)
     return prediction
+
+
+@app.get('/')
+def preview():
+    return {'hello world': 'House Price Prediction System API [v1].'}
 
 
 @app.post('/predict')
